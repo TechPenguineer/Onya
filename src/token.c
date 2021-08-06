651b7373a3f87efcd42570d2f6244ef2daebbed2
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-06 19:22:47 -0400

    remove workflows

--- src/token.c
+++ src/token.c
@@ -1,12 +1,17 @@
 #include "include/token.h"
 #include <stdlib.h>
 
+
 token_T* init_token(int type, char* value)
 {
-  token_T* token = calloc(1, sizeof(struct TOKEN_STRUCT));
-  token->type = type;
-  token->value = value;
+    token_T* token = calloc(1, sizeof(struct TOKEN_STRUCT));
+    token->type = type;
+    token->value = value;
 
+<<<<<<< HEAD
     
     token;
+=======
+    return token;
+>>>>>>> parent of e7b217b (make your code work)
 }
--- src/visitor.c
+++ src/visitor.c
@@ -3,113 +3,130 @@
 #include <stdio.h>
 #include <string.h>
 
+
 static AST_T* builtin_function_print(visitor_T* visitor, AST_T** args, int args_size)
-{
-  int i;
-  for (i = 0; i < args_size; i++) {
-    AST_T* visited_ast = visitor_visit(visitor, args[i]);
+{   int i;
+    for ( i = 0; i < args_size; i++)
+    {
+        AST_T* visited_ast = visitor_visit(visitor, args[i]);
 
-    switch (visited_ast->type) {
-      case AST_STRING: printf("%s\n", visited_ast->string_value); break;
-      default: printf("%p\n", visited_ast); break;
+        switch (visited_ast->type)
+        {
+            case AST_STRING: printf("%s\n", visited_ast->string_value); break;
+            default: printf("%p\n", visited_ast); break;
+        }
     }
-  }
 
-  return init_ast(AST_NOOP);
+    return init_ast(AST_NOOP);
 }
 
 static AST_T* builtin_function_exit(visitor_T* visitor, AST_T** args, int args_size)
-{
-  int i;
-  for (i = 0; i < args_size; i++) {
-    AST_T* visited_ast = visitor_visit(visitor, args[i]);
+{   
+    int i;
+    for ( i = 0; i < args_size; i++)
+    {
+        AST_T* visited_ast = visitor_visit(visitor, args[i]);
 
-    switch (visited_ast->type) {
-      case AST_NOOP:
-        printf("You exited\n");
-        exit(0);
-        break;
-      default: printf("%p\n", visited_ast); break;
+        switch (visited_ast->type)
+        {
+            case AST_NOOP: printf("You exited\n"); exit(0); break;
+            default: printf("%p\n", visited_ast); break;
+        }
     }
-  }
 
-  return init_ast(AST_NOOP);
+    return init_ast(AST_NOOP);
 }
 
 static AST_T* builtin_function_clear(visitor_T* visitor, AST_T** args, int args_size)
 {
-  int i;
-  for (i = 0; i < args_size; i++) {
-    AST_T* visited_ast = visitor_visit(visitor, args[i]);
+    int i;
+    for ( i = 0; i < args_size; i++)
+    {
+        AST_T* visited_ast = visitor_visit(visitor, args[i]);
 
-    switch (visited_ast->type) {
-      case AST_NOOP: system("clear"); break;
-      default: printf("%p\n", visited_ast); break;
+        switch (visited_ast->type)
+        {
+            case AST_NOOP: system("clear"); break;
+            default: printf("%p\n", visited_ast); break;
+        }
     }
-  }
 
-  return init_ast(AST_NOOP);
+    return init_ast(AST_NOOP);
 }
 
 visitor_T* init_visitor()
 {
-  visitor_T* visitor = calloc(1, sizeof(struct VISITOR_STRUCT));
+    visitor_T* visitor = calloc(1, sizeof(struct VISITOR_STRUCT));
 
-  return visitor;
+    return visitor;
 }
 
 AST_T* visitor_visit(visitor_T* visitor, AST_T* node)
 {
-  switch (node->type) {
-    case AST_VARIABLE_DEFINITION: return visitor_visit_variable_definition(visitor, node); break;
-    case AST_FUNCTION_DEFINITION: return visitor_visit_function_definition(visitor, node); break;
-    case AST_VARIABLE: return visitor_visit_variable(visitor, node); break;
-    case AST_FUNCTION_CALL: return visitor_visit_function_call(visitor, node); break;
-    case AST_STRING: return visitor_visit_string(visitor, node); break;
-    case AST_COMPOUND: return visitor_visit_compound(visitor, node); break;
-    case AST_NOOP: return node; break;
-  }
+    switch (node->type)
+    {
+        case AST_VARIABLE_DEFINITION: return visitor_visit_variable_definition(visitor, node); break;
+        case AST_FUNCTION_DEFINITION: return visitor_visit_function_definition(visitor, node); break;
+        case AST_VARIABLE: return visitor_visit_variable(visitor, node); break;
+        case AST_FUNCTION_CALL: return visitor_visit_function_call(visitor, node); break;
+        case AST_STRING: return visitor_visit_string(visitor, node); break;
+        case AST_COMPOUND: return visitor_visit_compound(visitor, node); break;
+        case AST_NOOP: return node; break;
+    }
 
-  printf("Uncaught statement of type `%d`\n", node->type);
-  exit(1);
+    printf("Uncaught statement of type `%d`\n", node->type);
+    exit(1);
 
-  return init_ast(AST_NOOP);
+    return init_ast(AST_NOOP);
 }
 
 AST_T* visitor_visit_variable_definition(visitor_T* visitor, AST_T* node)
 {
-  scope_add_variable_definition(node->scope, node);
+    scope_add_variable_definition(
+        node->scope,
+        node        
+    ); 
 
-  return node;
+    return node;
 }
 
 AST_T* visitor_visit_function_definition(visitor_T* visitor, AST_T* node)
 {
-  scope_add_function_definition(node->scope, node);
+    scope_add_function_definition(
+        node->scope,
+        node        
+    );
 
-  return node;
+    return node;
 }
 
 AST_T* visitor_visit_variable(visitor_T* visitor, AST_T* node)
 {
-  AST_T* vdef = scope_get_variable_definition(node->scope, node->variable_name);
+    AST_T* vdef = scope_get_variable_definition(
+        node->scope,
+        node->variable_name
+    );
+    
+    if (vdef != (void*) 0)
+        return visitor_visit(visitor, vdef->variable_definition_value);
 
-  if (vdef != (void*)0)
-    return visitor_visit(visitor, vdef->variable_definition_value);
-
-  printf("Undefined variable `%s`\n", node->variable_name);
-  exit(1);
+    printf("Undefined variable `%s`\n", node->variable_name);
+    exit(1);
 }
 
 AST_T* visitor_visit_function_call(visitor_T* visitor, AST_T* node)
 {
     if (strcmp(node->function_call_name, "print") == 0)
     {
+<<<<<<< HEAD
         return builtin_function_print(visitor, node->function_c
             
             
             
             ll_arguments, node->function_call_arguments_size);
+=======
+        return builtin_function_print(visitor, node->function_call_arguments, node->function_call_arguments_size);
+>>>>>>> parent of e7b217b (make your code work)
     }
 
     if (strcmp(node->function_call_name, "exit"||"quit") == 0)
@@ -160,15 +177,16 @@ AST_T* visitor_visit_function_call(visitor_T* visitor, AST_T* node)
 
 AST_T* visitor_visit_string(visitor_T* visitor, AST_T* node)
 {
-  return node;
+    return node;
 }
 
 AST_T* visitor_visit_compound(visitor_T* visitor, AST_T* node)
 {
-  int i;
-  for (i = 0; i < node->compound_size; i++) {
-    visitor_visit(visitor, node->compound_value[i]);
-  }
+    int i;
+    for ( i = 0; i < node->compound_size; i++)
+    {
+        visitor_visit(visitor, node->compound_value[i]);
+    }
 
-  return init_ast(AST_NOOP);
+    return init_ast(AST_NOOP);
 }
