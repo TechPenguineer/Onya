ad6467681b11e336f36f4c569224dc8ae74453c0
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-06 11:35:03 -0400

    define token

--- src/include/lexer.h
+++ src/include/lexer.h
@@ -1,12 +1,24 @@
 #ifndef LEXER_H
 #define LEXER_H 
+#include "token.h"
 
 typedef struct LEXER_STRUCT
 {
     char c; 
-    usigned
+    unsigned int i;
+    char* constents;
 
 } lexer_T;
+
+lexer_T8 init_lexer(char* contents);
+
+void lexer_advance(lexer_T* lexer);
+
+void lexer_skip_whitespace(lexer_T* lexer);
+
+token_T* lexer_get_next_token(lexer_T* lexer);
+
+token_T* lexer_collect_string(lexer_T* lexer)
 
 
 #endif
