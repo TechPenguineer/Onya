3840bc7bcdabf367e94abdb8f86157481774d972
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-06 12:54:31 -0400

    setup test parser

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include "include/lexer.h"
 int main(int argc, char* argv[])
 {
-	printf("Hello World!\n");
+	lexer_T* lexer = init_lexer(
+		"var name = \"BOB\";"
+		"print(name);"
+	);
+
+	token_T* token = (void*)0;
+
+	while ((token = lexer_get_next_token(lexer)) != (void*)0)
+	{
+		printf("TOKEN(%d,%s)\n", token->type, token->value);
+	}
 	return 0;
 }
