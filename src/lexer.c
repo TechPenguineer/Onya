8ac0a7f8066574d835c777c736ee2ab9258cc269
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-06 14:40:58 -0400

    lexer rewrite

--- src/lexer.c
+++ src/lexer.c
@@ -1,131 +1,113 @@
 #include "include/lexer.h"
-#include "include/token.h"
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdio.h>
+
+
 lexer_T* init_lexer(char* contents)
 {
-	//printf("CREATED INIT LEXER\n");
-	lexer_T* lexer = calloc(1, sizeof(struct LEXER_STRUCT));
-	lexer->contents = contents;
-	lexer->i = 0;
-	lexer->c = contents[lexer->i];
+    lexer_T* lexer = calloc(1, sizeof(struct LEXER_STRUCT));
+    lexer->contents = contents;
+    lexer->i = 0;
+    lexer->c = contents[lexer->i];
 
-	return lexer;
+    return lexer;
 }
 
 void lexer_advance(lexer_T* lexer)
 {
-	//printf("CREATED LEXER ADVANCER\n");
-
-	if (lexer->c != '\0' && lexer->i < strlen(lexer->contents))
-	{
-		lexer->i += 1;
-		lexer->c=lexer->contents[lexer->i];
-	}
+    if (lexer->c != '\0' && lexer->i < strlen(lexer->contents))
+    {
+        lexer->i += 1;
+        lexer->c = lexer->contents[lexer->i];
+    }
 }
+
 void lexer_skip_whitespace(lexer_T* lexer)
 {
-	//printf("CREATED SKIP WHITESPACES\n");
-
-	while (lexer->c == ' ' || lexer->c == 10)
-	{
-		lexer_advance(lexer);
-	}
+    while (lexer->c == ' ' || lexer->c == 10)
+    {
+        lexer_advance(lexer);
+    }
 }
 
 token_T* lexer_get_next_token(lexer_T* lexer)
 {
-	while (lexer->c != '\0' && lexer->i < strlen(lexer->contents))
-	{
-		//printf("NEW TOKEN\n");
-		
-		if (lexer->c == ' ' || lexer->c == 10)
-		{
-			lexer_skip_whitespace(lexer);
-			//printf("SKIPPED WHITESPACE\n");
+    while (lexer->c != '\0' && lexer->i < strlen(lexer->contents))
+    {
+        if (lexer->c == ' ' || lexer->c == 10)
+            lexer_skip_whitespace(lexer);
 
-		}
+        if (isalnum(lexer->c))
+            return lexer_collect_id(lexer);
 
-		if (isalnum(lexer->c))
-		{
-			return lexer_collect_id(lexer);
-			//printf("COLLECTED ID\n");
+        if (lexer->c == '"')
+            return lexer_collect_string(lexer);
 
-		}
+        switch (lexer->c)
+        {
+        case '=': return lexer_advance_with_token(lexer, init_token(TOKEN_EQUALS, lexer_get_current_char_as_string(lexer))); break;
+        case ';': return lexer_advance_with_token(lexer, init_token(TOKEN_SEMI, lexer_get_current_char_as_string(lexer))); break;
+        case '(': return lexer_advance_with_token(lexer, init_token(TOKEN_LPAREN, lexer_get_current_char_as_string(lexer))); break;
+        case ')': return lexer_advance_with_token(lexer, init_token(TOKEN_RPAREN, lexer_get_current_char_as_string(lexer))); break;
+  
+        }
+    }
 
-		// DIDNT RETURN V
-		if (lexer->c == '"')
-		{
-			return lexer_collect_string(lexer);
-			//printf("COLLECTED STRING\n");
-
-		}
-		switch (lexer->c)
-		{
-			case '=': return lexer_advance_with_token(lexer, init_token(TOKEN_EQUALS, lexer_get_current_char_as_string(lexer))); break;
-			case ';': return lexer_advance_with_token(lexer, init_token(TOKEN_SEMI, lexer_get_current_char_as_string(lexer))); break;
-			case '(': return lexer_advance_with_token(lexer, init_token(TOKEN_LPAREN, lexer_get_current_char_as_string(lexer))); break;
-			case ')': return lexer_advance_with_token(lexer, init_token(TOKEN_RPAREN, lexer_get_current_char_as_string(lexer))); break;
-
-		}
-	}
-		return(void*)0;
-	}
-
+    return init_token(TOKEN_EOF, "\0");
+}
 
 token_T* lexer_collect_string(lexer_T* lexer)
 {
-	char* value = calloc(1, sizeof(char));
-	value[0] = '\0';
+    lexer_advance(lexer);
 
-	while (lexer->c != '"')
-	{
-		//printf("CREATED STRING COLLECTOR\n");
-		//                 CHECK IN DEBUG V
-		char* s = lexer_get_current_char_as_string(lexer);
-		value = realloc(value, (strlen(value) + strlen(s) + 1) * sizeof(char));
-		strcat(value, s);
-		lexer_advance(lexer);
-	}
-	lexer_advance(lexer);
+    char* value = calloc(1, sizeof(char));
+    value[0] = '\0';
 
-	return init_token(TOKEN_STRING, value);
+    while (lexer->c != '"')
+    {
+        char* s = lexer_get_current_char_as_string(lexer);
+        value = realloc(value, (strlen(value) + strlen(s) + 1) * sizeof(char));
+        strcat(value, s);
+
+        lexer_advance(lexer);
+    }
+
+    lexer_advance(lexer);
+
+    return init_token(TOKEN_STRING, value);
 }
+
 token_T* lexer_collect_id(lexer_T* lexer)
 {
-	char* value = calloc(1, sizeof(char));
-	value[0] = '\0';
+    char* value = calloc(1, sizeof(char));
+    value[0] = '\0';
 
-	while (isalnum( lexer->c ))
-	{
-		//printf("COLLECTED ID\n");
+    while (isalnum(lexer->c))
+    {
+        char* s = lexer_get_current_char_as_string(lexer);
+        value = realloc(value, (strlen(value) + strlen(s) + 1) * sizeof(char));
+        strcat(value, s);
 
-		char* s = lexer_get_current_char_as_string(lexer);
-		value = realloc(value, (strlen(value) + strlen(s) + 1) * sizeof(char));
-		strcat(value, s);
-		lexer_advance(lexer);
-	}
-	lexer_advance(lexer);
+        lexer_advance(lexer);
+    }
 
-	return init_token(TOKEN_ID, value);
+    return init_token(TOKEN_ID, value);
 }
 
 token_T* lexer_advance_with_token(lexer_T* lexer, token_T* token)
 {
-	lexer_advance(lexer);
-	//printf("ADVANNCED TOKEN\n");
+    lexer_advance(lexer);
 
-	return token;
+    return token;
 }
 
 char* lexer_get_current_char_as_string(lexer_T* lexer)
 {
-	//printf("GOT CURRENT CHAR AS A STRNG\n");
+    char* str = calloc(2, sizeof(char));
+    str[0] = lexer->c;
+    str[1] = '\0';
 
-	char* str = calloc(2, sizeof(char));
-	str[0] = lexer->c;
-	str[1] = '\0';
-	return str;
+    return str;
 }
