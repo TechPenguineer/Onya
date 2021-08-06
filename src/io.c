e7b217bfefb95136f71a740659764f57cadba273
Sebastian Karlsson <ianertson@gmail.com>
2021-08-07 01:01:01 +0200

    make your code work

--- src/io.c
+++ src/io.c
@@ -1,30 +1,28 @@
 #include "include/io.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 
 char* get_file_contents(const char* filepath)
 {
-    char* buffer = 0;
-    long length;
+  char* buffer = 0;
+  long length;
 
-    FILE* f = fopen(filepath, "rb");
+  FILE* f = fopen(filepath, "rb");
 
-    if (f)
-    {
-        fseek(f, 0, SEEK_END);
-        length = ftell(f);
-        fseek(f, 0, SEEK_SET);
+  if (f) {
+    fseek(f, 0, SEEK_END);
+    length = ftell(f);
+    fseek(f, 0, SEEK_SET);
 
-        buffer = calloc(length, length);
+    buffer = calloc(length, length);
 
-        if (buffer)
-            fread(buffer, 1, length, f);
+    if (buffer)
+      fread(buffer, 1, length, f);
 
-        fclose(f);
-        return buffer;
-    }
+    fclose(f);
+    return buffer;
+  }
 
-    printf("Error reading file %s\n", filepath);
-    exit(2);
+  printf("Error reading file %s\n", filepath);
+  exit(2);
 }
--- src/main.c
+++ src/main.c
@@ -1,51 +1,93 @@
 
-#include <stdio.h>
-#include <string.h>
+#include "include/io.h"
 #include "include/lexer.h"
 #include "include/parser.h"
 #include "include/visitor.h"
-#include "include/io.h"
+#include <stdio.h>
+#include <string.h>
 
 #define MAX_LIMIT 20
 void print_help()
 {
-    printf("Usage:\nhello.out <filename>\n");
-    exit(1);
+  printf("Usage:\nhello.out <filename>\n");
+  exit(1);
+}
+
+char** split(const char* text, const char* deli, int* len)
+{
+
+  char** parts = 0;
+  char* fname = strdup(text);
+  char* tok = strtok(fname, deli);
+  int l = 0;
+
+  while (tok != 0) {
+    l++;
+    parts = realloc(parts, (l * sizeof(char*)) + (strlen(tok) * sizeof(char) + 1));
+    parts[l - 1] = strdup(tok);
+    tok = strtok(0, ".");
+  }
+
+  *len = l;
+
+  free(fname);
+
+  return parts;
+}
+
+unsigned int contains(char** parts, const char* needle, int len)
+{
+  for (int i = 0; i < len; i++)
+    if (strcmp(parts[i], needle) == 0)
+      return 1;
+
+  return 0;
+}
+
+unsigned int has_ext(const char* filename, const char* ext)
+{
+  int len = 0;
+  char** parts = split(filename, ".", &len);
+  unsigned int c = contains(parts, ext, len);
+
+  if (parts) {
+    for (int i = 0; i < len; i++)
+      free(parts[i]);
+
+    free(parts);
+  }
+
+  return c;
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc >= 2){
-        int i;
-        for( i = 1; i < argc; i++){
-            int len = strlen(argv[i]);
-            char* last_four = &argv[i][len-6];
-            if(strcmp(last_four,".onya") == 0){
-                                lexer_T* lexer = init_lexer(
-                    get_file_contents(argv[i])
-                );
-                parser_T* parser = init_parser(lexer);
-                AST_T* root = parser_parse(parser, parser->scope);
-                visitor_T* visitor = init_visitor();
-                visitor_visit(visitor, root);
-            }
+  if (argc >= 2) {
+    int i;
+    for (i = 1; i < argc; i++) {
+      if (has_ext(argv[i], "onya")) {
+        lexer_T* lexer = init_lexer(get_file_contents(argv[i]));
+        parser_T* parser = init_parser(lexer);
+        AST_T* root = parser_parse(parser, parser->scope);
+        visitor_T* visitor = init_visitor();
+        visitor_visit(visitor, root);
+      }
 
-            else {
-                print_help();
-            }
-        }
+      else {
+        print_help();
+      }
     }
-    else {
-        char input[MAX_LIMIT];
-        while(1){
-            printf("Welcome to the Onya language v. 0.0.1!\nCreated by Tech Penguineer\n>>> ");
-            fgets(input,MAX_LIMIT, stdin);
-            lexer_T* lexer = init_lexer(input);
-            parser_T* parser = init_parser(lexer);
-            AST_T* root = parser_parse(parser, parser->scope);
-            visitor_T* visitor = init_visitor();
-            visitor_visit(visitor, root);
-        }
+  } else {
+    char input[MAX_LIMIT];
+    while (1) {
+      printf("Welcome to the Onya language v. 0.0.1!\nCreated by Tech Penguineer\n>>> ");
+      fgets(input, MAX_LIMIT, stdin);
+      lexer_T* lexer = init_lexer(input);
+      parser_T* parser = init_parser(lexer);
+      AST_T* root = parser_parse(parser, parser->scope);
+      visitor_T* visitor = init_visitor();
+      visitor_visit(visitor, root);
     }
-    return 0;
+  }
+  return 0;
 }
